41bd71a1e2ed398ae7af31c1289d5fec5868941c
Vajira Rathnayaka <158836060+VajiraTech@users.noreply.github.com>
2024-02-17 23:13:50 +0530

    Add files via upload

--- lib/start.sh
+++ lib/start.sh
@@ -1,5 +1,5 @@
 while true
 do
-echo "Starting suhail-md..."
+echo "Starting Mac-Md..."
 node lib/client.js
 done
--- lib/ttt.d.ts
+++ lib/ttt.d.ts
@@ -1,6 +1,9 @@
 export declare class TicTacToe {
+    /* X PlayerName */
     playerX: string;
+    /* Y PlayerName */
     playerY: string;
+    /* X if true, Y if false */
     _currentTurn: boolean;
     _x: number;
     _y: number;
